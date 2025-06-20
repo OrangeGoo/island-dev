dbc554d36ebb0b9f4e148079abeda6c3492efe73
OrangeGoo <gjc20021013@gmail.com>
2025-06-20 17:17:19 +0800

    feat: route-01

--- src/runtime/client-entry.tsx
+++ src/runtime/client-entry.tsx
@@ -1,12 +1,15 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import siteData from 'island:site-data';
+import { BrowserRouter } from 'react-router-dom';
 
 function renderInBrower() {
-  console.log(siteData);
   const containerEl = document.getElementById('root');
   if (!containerEl) throw new Error('#Error element not found');
-  createRoot(containerEl).render(<App />);
+  createRoot(containerEl).render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
 }
 
 renderInBrower();
