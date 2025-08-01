b5c6d91363fe153d0179f3971ed201ac09920bec
OrangeGoo <gjc20021013@gmail.com>
2025-08-01 18:18:29 +0800

    feat: island-bundle-dev

--- src/runtime/ssr-entry.tsx
+++ src/runtime/ssr-entry.tsx
@@ -3,17 +3,29 @@ import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom/server';
 import { DataContext } from './hooks';
 
+export interface RenderResult {
+  appHtml: string;
+  islandToPathMap: Record<string, string>;
+  islandProps: unknown[];
+}
+
 export async function render(pagePath: string) {
   const pageData = await initPageData(pagePath);
-  const { clearIslandData } = await import('./jsx-runtime');
+  const { clearIslandData, data } = await import('./jsx-runtime');
+  const { islandToPathMap, islandProps } = data;
   clearIslandData();
-  return renderToString(
+  const appHtml = renderToString(
     <DataContext.Provider value={pageData}>
       <StaticRouter location={pagePath}>
         <App />
       </StaticRouter>
     </DataContext.Provider>
   );
+  return {
+    appHtml,
+    islandToPathMap,
+    islandProps
+  };
 }
 
 export { routes } from 'island:routes';
